dc16e76bb818c2d59825bbc958bf6ecf13ea5125
|GriB28| <gp.off.dev@gmail.com>
2026-04-06 14:47:41 +0300

    [base-test] выгрузка static в сборку

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from source.test import router as test_router
 
 
 app = FastAPI()
 app.include_router(test_router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
