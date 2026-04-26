9f9590c1b7807f01b1bb866bebe067fccff61ab6
wiheg <tmandzhiev1@gmail.com>
2026-04-26 18:31:11 +0300

    unsafe version

--- source/auth/utils.py
+++ source/auth/utils.py
@@ -1,5 +1,23 @@
 from LyPayAPI.user.registration import check_email_record, send_email, new
 from LyPayAPI.user.info import get_by_email
+from LyPayAPI.__exceptions__ import APIError
+
+
+async def send_verification_code(email: str, code: str) -> None:
+    """
+    Проверяет, не занят ли email, и отправляет на него код подтверждения.
+    """
+    await check_email_record(email)
+    await send_email(route="main", participant=email, code=code)
+
+
+async def verify_code(email: str, code: str, ref_code: str) -> bool:
+    """
+    Проверяет код подтверждения для указанного email.
+    """
+    if code == ref_code:
+        return True
+    raise APIError()  # todo: выдать ошибку
 
 
 async def register_user(email: str, password: str, login: str):
@@ -9,28 +27,20 @@ async def register_user(email: str, password: str, login: str):
     """
     user_info = await check_email_record(email)
     print(user_info)
-    code = "123"  # todo:сделать проверку почты
-    await send_email(route="main", participant=email, code=code)
-
-    # Создаём пользователя
     user_id = await new(
         email=email,
         login=login,
         password=password,
         name=user_info["name"],
-        group=user_info["class"],
+        group=user_info["group"],
         owner_flag="web_owner"
     )
-    print(user_id)
     return user_id
 
 
 async def authenticate_user(email: str, password: str):
     """
     Проверяет учётные данные пользователя.
-    Так как прямого метода /login в API нет, мы получаем информацию о пользователе
-    по email (предполагается, что email уникален) и сверяем пароль.
-    В реальном API нужно добавить конечную точку аутентификации.
     """
     user_data = await get_by_email(email)
     print(user_data)
