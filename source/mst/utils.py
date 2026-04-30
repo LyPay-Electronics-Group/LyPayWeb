48a581948b02c22cf157c776b2a66cd378377a38
|GriB28| <gp.off.dev@gmail.com>
2026-04-30 12:10:27 +0300

    test 3 in utils

--- source/mst/utils.py
+++ source/mst/utils.py
@@ -3,6 +3,9 @@ from LyPayAPI.mst import test2 as mst_test2
 from LyPayAPI.mst import test3 as mst_test3
 
 
+GLOBAL_TEST3_PARAMETER = 10
+
+
 async def test1() -> bool:
     """
     Full-stack первый тест
@@ -13,3 +16,25 @@ async def test1() -> bool:
     hash_from_core, og_hash = await mst_test1.main()
 
     return hash_from_core == og_hash
+
+
+async def test3(ID: int) -> None:
+    """
+    Full-stack первый тест
+
+    :param ID: ID текущего пользователя
+    :return: параметр успешности теста (True/False)
+    """
+
+    await mst_test3.main(ID)
+
+
+async def test3_end(ID: int) -> bool:
+    """
+    Завершение третьего теста
+
+    :param ID: ID текущего пользователя
+    :return: параметр успешности теста (True/False)
+    """
+
+    return await mst_test3.end(ID)
