8a907fc52ae7babaf86a4fb96c2745b814395b99
|GriB28| <gp.off.dev@gmail.com>
2026-04-30 12:16:12 +0300

    mst complete (for test1 and test3) build

--- source/mst/__init__.py
+++ source/mst/__init__.py
@@ -1,1 +1,11 @@
-from .router import router
+from fastapi import APIRouter
+
+from .router import router as main_router
+from .test1 import router as test1_router
+from .test3 import router as test3_router
+
+
+router = APIRouter()
+router.include_router(main_router)
+router.include_router(test1_router)
+router.include_router(test3_router)
