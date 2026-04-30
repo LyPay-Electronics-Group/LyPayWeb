6620c7353b36257843197e6bde0f62d24a6c5d6a
|GriB28| <gp.off.dev@gmail.com>
2026-04-30 12:09:02 +0300

    mst test 3: prototype

---
+++ source/mst/test3.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, Request
+
+from asyncio import sleep
+from random import randint
+
+from scripts.unix import unix
+
+from .utils import test3, test3_end, GLOBAL_TEST3_PARAMETER
+
+router = APIRouter()
+
+
+@router.get("/test3")
+async def do_test3(request: Request):
+    if request.session.get("user") is None:
+        pass  # TODO: проверка регистрации
+    ID = request.session.get("user")["ID"]
+
+    start_time = unix()
+    delta_time = 0
+    for _ in range(GLOBAL_TEST3_PARAMETER):
+        await test3(ID)
+
+        delta_time_start = unix()
+        await sleep(randint(10, 20) / 1000)
+        delta_time_end = unix()
+
+        delta_time += delta_time_end - delta_time_start
+    end_time = unix()
+
+    if request.session.get("mst") is None:
+        request.session["mst"] = dict()
+
+    if request.session.get("mst").get("test3") is None:
+        request.session["mst"]["test3"] = {
+            "total": GLOBAL_TEST3_PARAMETER,
+            "time": end_time - start_time - delta_time,
+        }
+    else:
+        request.session["mst"]["test3"]["total"] += GLOBAL_TEST3_PARAMETER
+        request.session["mst"]["test3"]["time"] += end_time - start_time - delta_time
+
+
+@router.get("/test3_end")
+async def end_test3(request: Request):
+    if request.session.get("user") is None:
+        pass  # TODO: проверка регистрации
+
+
+    result_local, result_core = await test3_end(request.session.get("user")["ID"])
+
+    if request.session.get("mst") is not None and request.session.get("mst").get("test3") is not None:
+        request.session["mst"]["test3"]["local"] = result_local
+        request.session["mst"]["test3"]["core"] = result_core
