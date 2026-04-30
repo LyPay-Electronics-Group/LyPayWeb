c8959eb205276cc60f1d25b1251e8634a017c326
|GriB28| <gp.off.dev@gmail.com>
2026-04-30 21:08:51 +0300

    added the custom logger

--- server.py
+++ server.py
@@ -7,9 +7,22 @@ from source.test import router as test_router
 from source.auth import router as auth_router
 from source.mst import router as mst_router
 
+from logging import getLogger, StreamHandler
+from sys import stdout
+from middleware.logger import CustomLog
+
 app = FastAPI()
 
+logger = getLogger("app.requests")
+logger.setLevel(20)  # level INFO
+logger.addHandler(StreamHandler(stdout))
+
+app.add_middleware(CustomLog, app_logger=logger, blacklist=[
+    "/mst/machine/local_stats",
+    "/mst/machine/core_stats"
+])
 app.add_middleware(SessionMiddleware, secret_key="verysecretkey")  # todo:брать из .env
+
 app.include_router(test_router)
 app.include_router(auth_router)
 app.include_router(mst_router, prefix='/mst')
