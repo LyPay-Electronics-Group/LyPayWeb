039e1ba4f0d0caad002014394e7217e419bdda18
|GriB28| <gp.off.dev@gmail.com>
2026-05-04 19:26:18 +0300

    new simple launcher for async processing

--- launcher.py
+++ launcher.py
@@ -1,5 +1,24 @@
-from uvicorn import run
-from server import app
+from os import system, getenv
+from platform import system as get_platform
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-run(app, host='26.251.171.35', port=12345, access_log=False)
+if get_platform() == "Windows":
+    system(
+        f"uvicorn server:app "
+        "--no-access-log "
+        f"--workers $NUMBER_OF_PROCESSORS "
+        f"--host {getenv('LYPAY_HOST')} "
+        f"--port {getenv('LYPAY_PORT')}"
+   )
+
+elif get_platform() == "Linux":
+    system(
+        f"uvicorn server:app "
+        "--no-access-log "
+        f"--workers $(nproc) "
+        f"--host {getenv('LYPAY_HOST')} "
+        f"--port {getenv('LYPAY_PORT')}"
+    )
