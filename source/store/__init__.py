774b6e22484dcf441eb245447cbdd9f101fc8f83
|GriB28| <gp.off.dev@gmail.com>
2026-05-12 12:12:31 +0300

    items' router, displaying and editing (which is not affecting anything yet)

--- source/store/__init__.py
+++ source/store/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .register import router as register_router
 from .access import router as access_router
+from .items import router as items_settings_router
 from .index import router as base_router
 
 
@@ -9,3 +10,4 @@ router = APIRouter()
 router.include_router(base_router)
 router.include_router(register_router, prefix="/register")
 router.include_router(access_router, prefix="/access")
+router.include_router(items_settings_router, prefix="/items")
---
+++ source/store/items.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter, Request, Depends as D
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+
+from scripts.firewall_validator import firewall_validate_factory as FVF
+
+from LyPayAPI.store.info import get, get_by_shopkeeper
+from LyPayAPI.store import items
+from LyPayAPI.__exceptions__ import IDNotFound, UserIsAlreadyShopkeeper
+
+router = APIRouter()
+templates = Jinja2Templates(directory="html/store/items")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def items_page(
+        request: Request,
+        firewall_ok = D(FVF('stores'))
+):
+    if not firewall_ok:
+        return RedirectResponse("/", status_code=303)
+
+    user_info = request.session.get("user", None)
+    if user_info is None:
+        return RedirectResponse("/login", status_code=303)
+
+    current_storeID = await get_by_shopkeeper(user_info["ID"])
+
+    return templates.TemplateResponse(
+        "items.html",
+        {
+            "request": request,
+            "store": await get(current_storeID),
+            "items": [await items.get(item) for item in await items.get_all(current_storeID, active_filter=True)],
+        }
+    )
+
+
+@router.get("/add")
+async def add(
+        ID: int,
+        request: Request,
+        firewall_ok = D(FVF('stores'))
+):
+    if not firewall_ok:
+        return RedirectResponse("/", status_code=303)
+
+    user_info = request.session.get("user", None)
+    if user_info is None:
+        return RedirectResponse("/login", status_code=303)
+
+    current_storeID = await get_by_shopkeeper(user_info["ID"])
+    if (await get(current_storeID))["hostID"] != user_info["ID"]:
+        return RedirectResponse("/store", status_code=303)
+
+    if ID == user_info["ID"]:
+        return JSONResponse({"error": True}, status_code=403)
+
+    try:
+        await items.add(current_storeID, "test", 0)
+        return JSONResponse({"ok": True}, status_code=200)
+    except IDNotFound, UserIsAlreadyShopkeeper:
+        return JSONResponse({"error": True}, status_code=403)
